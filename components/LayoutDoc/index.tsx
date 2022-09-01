4220fa3457a07305309343d2d22548578178adbd
ibelick <julien.thibeaut@gmail.com>
2022-09-01 11:52:42 +0200

    add @code-hike/mdx, init button.mdx

--- components/LayoutDoc/index.tsx
+++ components/LayoutDoc/index.tsx
@@ -3,7 +3,11 @@ type LayoutDocProps = {
 };
 
 const LayoutDoc: React.FC<LayoutDocProps> = ({ children }) => {
-  return <div className="prose">{children}</div>;
+  return (
+    <div className="max-w-2xl px-4 mx-auto my-10 prose xs:px-8 lg:max-w-3xl">
+      <article className="prose ">{children}</article>
+    </div>
+  );
 };
 
 export default LayoutDoc;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const theme = require("shiki/themes/nord.json");
+const { remarkCodeHike } = require("@code-hike/mdx");
 
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [],
+    remarkPlugins: [[remarkCodeHike, { theme }]],
     rehypePlugins: [],
     // If you use `MDXProvider`, uncomment the following line.
     // providerImportSource: "@mdx-js/react",
